ae617f5dc8d3d62e9806c963efe575e125beb4f9
Sugut <sugutvictorine@gmail.com>
2022-11-10 12:02:30 +0300

    changes done

---
+++ royal-vet-clinic/src/components/PatientCard.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
+import { Link } from "react-router-dom";
+
+
+function PatientCard({patient}) {
+    
+
+    return (
+        <div className="item"> 
+            <Card >
+                <Card.Body>
+                <Card.Title >{patient.name}</Card.Title>
+                <Card.Text> Animal Type: {patient.animal_type}
+                </Card.Text>
+                <Link to={`/allpatients/${patient.id}`}>
+                    <Button variant="primary" >Details</Button >
+                </Link>
+                </Card.Body>
+            </Card>
+        </div>
+    );
+}
+
+
+export default PatientCard;
