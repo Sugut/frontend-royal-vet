c776809f568ba36f4e728684b19f2ae195dd6bca
Sugut <sugutvictorine@gmail.com>
2022-11-10 12:00:35 +0300

    changes done

---
+++ royal-vet-clinic/src/components/Home.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Search from "./Search"
+
+function Home({allpatients}) {
+    return (
+        <div>
+        <h1 className='header'>Hello!</h1>
+        <p id='sub-header'>Which patient are we working on today?</p>
+        <Search patients={allpatients}/>
+        </div>
+    );
+}
+
+
+
+export default Home;
