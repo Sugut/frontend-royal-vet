8e41a00e777cbff1683caefc7d4ce93dd2df18ba
Sugut <sugutvictorine@gmail.com>
2022-11-10 12:03:22 +0300

    changes done

---
+++ royal-vet-clinic/src/components/Patients.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import PatientCard from './PatientCard';
+import CardGroup from 'react-bootstrap/CardGroup';
+
+
+
+function Patients({allpatients}) {
+
+    const renderPatients = allpatients.map((patient) => (
+        <div key={patient.id}>
+            <PatientCard patient={patient}/>
+        </div>
+    ));
+
+    return (
+        <div>
+            <h3 className='title'>Patients</h3>
+                <div className="container">
+                    <CardGroup > {renderPatients} </CardGroup>
+                </div>
+         </div>
+    );
+}
+
+export default Patients;
+
