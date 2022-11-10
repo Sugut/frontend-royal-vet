eafd2806450bcdb9964a3c1aa60b42cf1c8a7f9f
Sugut <sugutvictorine@gmail.com>
2022-11-10 12:04:34 +0300

    changes done

---
+++ royal-vet-clinic/src/components/Vets.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import VetCard from './VetCard';
+import Row from 'react-bootstrap/Row';
+
+
+function Vets({vets, appointments}) {
+
+
+    const renderVets = vets.map((vet) => (
+        <div key={vet.id} className="item">
+            <VetCard vet={vet} appointments={appointments}/>
+        </div>
+    ));
+
+    return (
+        <div >
+            <h3 className='title'>Veterinarians</h3>       
+            <Row md={2} className="justify-content-md-center" id="row">
+                {renderVets}
+            </Row>
+         </div>
+    );
+}
+
+export default Vets;
+
+
+
+
+
+
+
+
+
+
+
