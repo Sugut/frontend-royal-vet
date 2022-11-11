84584c5c187d6a5d9a6ce2bc6f700c002c19cda2
Sugut <sugutvictorine@gmail.com>
2022-11-11 10:29:02 +0300

    changes done

--- royal-vet-clinic/src/components/PatientCard.js
+++ royal-vet-clinic/src/components/PatientCard.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import Card from 'react-bootstrap/Card';
 // import Button from 'react-bootstrap/Button';
-// import { useNavigate } from "react-router-dom";
-// import Patients from './Patients';
+// import { Link } from "react-router-dom";
+
 
 
 function PatientCard({patient}) {
@@ -14,9 +14,9 @@ function PatientCard({patient}) {
                 <Card.Title >{patient.name}</Card.Title>
                 <Card.Text> Animal Type: {patient.animal_type}
                 </Card.Text>
-                {/* <Link to={`/allpatients/${patient.id}`}> */}
-                {/* <Button variant="primary">Details</Button > */}
-                {/* </Link> */}
+                {/* <Link to={`/allpatients/${patient.id}`}> 
+                 <Button variant="primary">Details</Button >
+                </Link> */}
                 </Card.Body>
             </Card>
         </div>
