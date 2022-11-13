14c17b8d8db8d5dd290a0592f274485844a36492
Sugut <sugutvictorine@gmail.com>
2022-11-13 11:56:14 +0300

    changes done

--- royal-vet-clinic/src/components/Footer.js
+++ royal-vet-clinic/src/components/Footer.js
@@ -6,6 +6,7 @@ function Footer() {
     <div className='footer'>
       <div className='footer_left'>
          <h5>Location</h5>
+         <p>TOM MBOYA STRRET, GREEN HOUSE, ROOM 104</p>
       </div>
       <div className='footer_center'>
          <h6>Contacts</h6>
